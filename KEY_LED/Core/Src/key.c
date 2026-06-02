dac91119b8227cedc859afddeca0f367089506e7
Link <3591564638@qq.com>
2026-06-02 16:25:10 +0800

    feat: 封装模块化按键驱动

---
+++ KEY_LED/Core/Src/key.c
@@ -0,0 +1,14 @@
+#include "key.h"
+
+uint8_t KEY_Scan(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin)
+{
+    if (HAL_GPIO_ReadPin(GPIOx, GPIO_Pin) == GPIO_PIN_RESET)
+    {
+        HAL_Delay(20);
+        if (HAL_GPIO_ReadPin(GPIOx, GPIO_Pin) == GPIO_PIN_RESET)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
